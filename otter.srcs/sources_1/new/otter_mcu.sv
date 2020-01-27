e55527d548162de10d45b089f17ea9742e5d454e
jarmeister99 <jarmeister99@gmail.com>
2020-01-26 16:45:20 -0800

    moved functionality from decoder to separate module

--- otter.srcs/sources_1/new/otter_mcu.sv
+++ otter.srcs/sources_1/new/otter_mcu.sv
@@ -33,6 +33,7 @@ typedef enum logic [6:0] {
 
 typedef struct packed{
     opcode_t opcode;
+    logic [2:0] func3;
     logic [4:0] rfAddr1;
     logic [4:0] rfAddr2;
     logic [4:0] rd;
@@ -77,10 +78,12 @@ wire  [31:0] ir;         // May need to be stage reg?
 wire  [1:0]  aluSrcB;
 wire         aluSrcA;
 logic [31:0] if_de_pc=0;
+logic [31:0] if_de_ir=0;
 
 always_ff @(posedge CLK) begin
     if (!stallDe) begin
         if_de_pc <= pc;
+        if_de_ir <= ir;
     end
 end
 
@@ -89,6 +92,7 @@ end
 // ~~~~~~~~~~~~~ //
 
 wire [31:0] aluAIn, aluBIn;
+logic [31:0] de_ex_ir=0;
 logic [31:0] de_ex_pc=0;
 logic [31:0] de_ex_aluAIn=0, de_ex_aluBIn=0;
 logic        brLt, brEq, brLtu;
@@ -96,100 +100,113 @@ inst_t de_ex_inst;
 
 always_ff @(posedge CLK) begin
     if (!stallEx) begin
-        de_ex_pc <= if_de_pc;
+        de_ex_ir     <= if_de_ir;
+        de_ex_pc     <= if_de_pc;
         de_ex_aluAIn <= aluAIn;
         de_ex_aluBIn <= aluBIn;
     end
+end
+
+always_comb begin
+    assign de_ex_inst.opcode = de_ex_ir[6:0];
+    assign de_ex_inst.func3  = de_ex_ir[14:12];
 end
 
 // ~~~~~~~~~~~~ //
 // MEMORY STAGE //
 // ~~~~~~~~~~~~ //
 
-
-wire [31:0] jalrPc, branchPc, jalPc;      // Set by Target Generator
-logic [1:0] ex_mem_pcSel=0;               // Set by Decoder
+wire  [31:0] jalrPc, branchPc, jalPc;      
+wire  [31:0] aluRes;
+wire   [1:0] pcSel;
+logic [31:0] ex_mem_jalrPc=0, ex_mem_branchPc=0, ex_mem_jalPc=0;
+logic [31:0] ex_mem_aluRes=0;
+logic  [1:0] ex_mem_pcSel=0;               
 inst_t ex_mem_inst;
 
-assign pcSel = ex_mem_pcSel;
+always_ff @(posedge CLK) begin
+    if (!stallMem) begin
+        ex_mem_pcSel    <= pcSel;
+        ex_mem_jalrPc   <= jalrPc;
+        ex_mem_branchPc <= branchPc;
+        ex_mem_jalPc    <= jalPc;
+    end
+end
 
+// ~~~~~~~~~~~~~~~ //
+// WRITEBACK STAGE //
+// ~~~~~~~~~~~~~~~ //
 
-
-
-
-// WRITEBACK STAGE
 wire [31:0] memData;     // Tied to MEM2 output
 
+// MODULES
+
 ProgCount prog_count(
-    .PC_CLK(CLK),    
-    .PC_RST(RESET),
-    .PC_LD(~stallIf),  // Always 1 unless we should stall
-    .PC_DIN(nextPc),   // Output of PC 4-1 MUX
-    .PC_COUNT(pc)
+    .PC_CLK    (CLK),    
+    .PC_RST    (RESET),
+    .PC_LD     (~stallIf),  // Always 1 unless we should stall
+    .PC_DIN    (nextPc),   // Output of PC 4-1 MUX
+    .PC_COUNT  (pc)
 );
 Mult4to1 prog_count_next_mux(
-    .In1(pc + 4),      // Default value 
-    .In2(jalrPc),      // Calculated at EX/MEM stage
-    .In3(branchPc),    // ...
-    .In4(jalPc),       // ...
-    .Sel(pcSel),       // Control signal set by EX/MEM stage, otherwise 0
-    .Out(nextPc)
+    .In1  (pc + 4),             
+    .In2  (ex_mem_jalrPc),             
+    .In3  (ex_mem_branchPc),          
+    .In4  (ex_mem_jalPc),            
+    .Sel  (ex_mem_pcSel),      
+    .Out  (nextPc)
 );    
 
 OTTER_mem_byte mem(
-    .MEM_CLK(CLK),
-    .MEM_ADDR1(pc),
-    .MEM_ADDR2(aluRes),               // May need to be stage reg?
-    .MEM_DIN2(ex_mem_inst.rs2),
-    .MEM_WRITE2(ex_mem_inst.memWrite),
-    .MEM_READ1(~stallIf),
-    .MEM_READ2(ex_mem_inst.memRead2),
-    .IO_IN(IOBUS_IN),
-    .MEM_SIZE(ex_mem_inst.memType[2:1]),
-    .MEM_SIGN(ex_mem_inst.memType[0]),
-    .ERR(0),
-    .MEM_DOUT1(ir),
-    .MEM_DOUT2(memData),
-    .IO_WR(IO_WR)
+    .MEM_CLK     (CLK),
+    .MEM_ADDR1   (pc),
+    .MEM_ADDR2   (ex_mem_aluRes),               
+    .MEM_DIN2    (ex_mem_inst.rs2),
+    .MEM_WRITE2  (ex_mem_inst.memWrite),
+    .MEM_READ1   (~stallIf),
+    .MEM_READ2   (ex_mem_inst.memRead2),
+    .IO_IN       (IOBUS_IN),
+    .MEM_SIZE    (ex_mem_inst.memType[2:1]),
+    .MEM_SIGN    (ex_mem_inst.memType[0]),
+    .ERR         (0),
+    .MEM_DOUT1   (ir),
+    .MEM_DOUT2   (memData),
+    .IO_WR       (IO_WR)
 );
 
 branch_cond_gen branch_cond_gen(
-    .A(de_ex_aluAIn),         
-    .B(de_ex_aluBIn),          
-    .BR_LT(brLt),
-    .BR_EQ(brEq),
-    .BR_LTU(brLtu)
+    .A       (de_ex_aluAIn),         
+    .B       (de_ex_aluBIn),          
+    .OPCODE  (de_ex_inst.opcode),
+    .FUNC3   (de_ex_inst.func3),
+    .PC_SEL  (pcSel)
 );
 
 OTTER_CU_Decoder decoder(
-    .CU_OPCODE(ir[6:0]),
-    .CU_FUNC3(ir[14:12]),
-    .CU_FUNC7(ir[31:25]),
-    .CU_BR_EQ(-0000),
-    .CU_BR_LT(-0000),
-    .CU_BR_LTU(-0000),
-    .CU_PCSOURCE(),          // TODO
-    .CU_ALU_SRCA(aluSrcA),
-    .CU_ALU_SRCB(aluSrcB),
-    .CU_ALU_FUN(),           // TODO
-    .CU_RF_WR_SEL()          // TODO
+    .CU_OPCODE     (if_de_ir[6:0]),
+    .CU_FUNC3      (if_de_ir[14:12]),
+    .CU_FUNC7      (if_de_ir[31:25]),
+    .CU_ALU_SRCA   (aluSrcA),
+    .CU_ALU_SRCB   (aluSrcB),
+    .CU_ALU_FUN    (),           // TODO
+    .CU_RF_WR_SEL  ()            // TODO
 );
     
 target_gen target_gen(
-    .RS1(rs1),            
-    .I_IMMED(iTypeImmed), // iTypeImmed received from Immediate Generator
-    .PC(de_ex_pc),        // Target generator must used saved PC
-    .IR(ir),              // Current ir can be used here?
-    .JALR_PC(jalrPc),
-    .BRANCH_PC(branchPc),
-    .JAL_C(jalPc)
+    .RS1        (rs1),            
+    .I_IMMED    (iTypeImmed), 
+    .PC         (de_ex_pc),       
+    .IR         (de_ex_ir),              
+    .JALR_PC    (jalrPc),
+    .BRANCH_PC  (branchPc),
+    .JAL_C      (jalPc)
 );
     
 immed_gen immed_gen(
-    .IR(ir),
-    .S_TYPE_IMMED(sTypeImmed),
-    .I_TYPE_IMMED(iTypeImmed),
-    .U_TYPE_IMMED(uTypeImmed)
+    .IR            (if_de_ir),
+    .S_TYPE_IMMED  (sTypeImmed),
+    .I_TYPE_IMMED  (iTypeImmed),
+    .U_TYPE_IMMED  (uTypeImmed)
 );
 
 endmodule
