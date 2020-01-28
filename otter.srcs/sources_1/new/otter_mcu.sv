4b1a38fcf1232444bdc1c5a16aabd39e76a512df
jarmeister99 <jarmeister99@gmail.com>
2020-01-28 10:50:36 -0800

    scooted regs

--- otter.srcs/sources_1/new/otter_mcu.sv
+++ otter.srcs/sources_1/new/otter_mcu.sv
@@ -46,8 +46,8 @@ typedef struct packed{
     logic memRead2;
     logic regWrite;
     logic [1:0] rfWrSel;
-    logic [2:0] memType;  //sign, size
     logic [31:0] rs1, rs2;
+    logic [31:0] pc;
 } instr_t;
 
 module otter_mcu(
@@ -67,76 +67,81 @@ logic invalidate;
 // FETCH STAGE //
 // ~~~~~~~~~~~ //
 
-wire  [31:0] pc, nextPc;
+wire  [31:0] pc, nextPc, ir;
+instr_t if_de_inst;
+
+// On each clock_edge, save the current PC into the IF_DE register
+always_ff @(posedge CLK) begin
+    if_de_inst.pc <= pc;
+end
 
 // ~~~~~~~~~~~~ //
 // DECODE STAGE //
 // ~~~~~~~~~~~~ //
 
+wire  [31:0] aluAIn, aluBIn; // The outputs from the 2to1 and 4to1 RF mux's
 wire  [31:0] sTypeImmed, iTypeImmed, uTypeImmed;
-wire  [31:0] ir;         // May need to be stage reg?
 wire  [31:0] rs1, rs2;
 wire   [3:0] aluFun;
 wire   [1:0] rfWrSel;
 wire   [1:0] aluSrcB;
 wire         aluSrcA;
-logic [31:0] if_de_pc=0;
-logic [31:0] if_de_ir=0;
 
-instr_t if_de_inst;
+instr_t de_inst;
 opcode_t opcode;
 
+instr_t de_ex_inst;
+logic [31:0] de_ex_iTypeImmed;
+logic [31:0] de_ex_aluAIn, de_ex_aluBIn;
+
+// Combinatorially populate de_inst structure with information from the instruction, and other combinatorial modules in the decode stage
+always_comb begin
+    de_inst.ir       = ir;
+    de_inst.pc       = if_de_inst.pc;
+    
+    opcode           = opcode_t'(de_inst.ir[6:0]);
+    de_inst.memWrite = opcode == STORE;
+    de_inst.memRead2 = opcode == LOAD;    // Make sure this is the right ir
+    de_inst.regWrite = de_inst.ir[6:0] != BRANCH &&
+                       de_inst.ir[6:0] != LOAD   &&           
+                       de_inst.ir[6:0] != STORE; 
+    de_inst.rd       = de_inst.ir[11:7];
+    de_inst.rfAddr1  = de_inst.ir[19:15];
+    de_inst.rdAddr2  = de_inst.ir[24:20];
+    de_inst.func3    = de_inst.ir[14:12]; // func3 is also memType
+    de_inst.opcode   = opcode;
+    de_inst.aluFun   = aluFun;     // received from decoder
+    de_inst.rfWrSel  = rfWrSel;    // received from decoder
+    de_inst.rs1      = rs1;        // received from reg
+    de_inst.rs2      = rs2;        // received from reg
+    de_inst.invalid  = invalidate; // received from hazard detector (THINK ABOUT THIS MORE, A LOT MORE)
+end
 always_ff @(posedge CLK) begin
+    // Save decoded instructions to DE_EX register if appropriate
     if (!stallDe) begin
-        if_de_pc            <= pc;     
-        if_de_ir            <= ir;
-        if_de_inst.opcode   <= opcode;
-        if_de_inst.aluFun   <= aluFun;
-        if_de_inst.rfWrSel  <= rfWrSel;
-        if_de_inst.rs1      <= rs1;
-        if_de_inst.rs2      <= rs2;
-        if_de_inst.invalid  <= invalidate;
-     
+        de_ex_inst       <= de_inst;
+        de_ex_aluAIn     <= aluAIn;
+        de_ex_aluBIn     <= aluBIn;
+        de_ex_iTypeImmed <= iTypeImmed;
     end
 end
-
-always_comb begin
-    opcode              = opcode_t'(ir[6:0]);
-    if_de_inst.rfAddr1  = ir[19:15];
-    if_de_inst.rfAddr2  = ir[24:20];
-    if_de_inst.func3    = ir[14:12];
-    if_de_inst.memType  = ir[14:12];
-    if_de_inst.rd       = ir[11:7];
-    if_de_inst.memWrite = ir[6:0] == STORE;
-    if_de_inst.memRead2 = ir[6:0] == LOAD;
-    if_de_inst.regWrite = ir[6:0] != BRANCH &&
-                          ir[6:0] != LOAD   &&            // How does this make sense?
-                          ir[6:0] != STORE; 
-end
-    
-
-
 
 // ~~~~~~~~~~~~~ //
 // EXECUTE STAGE //
 // ~~~~~~~~~~~~~ //
 
-wire  [31:0] aluAIn, aluBIn;
-logic [31:0] de_ex_iTypeImmed;
-logic [31:0] de_ex_ir=0;
-logic [31:0] de_ex_pc=0;
-logic [31:0] de_ex_aluAIn=0, de_ex_aluBIn=0;
-instr_t de_ex_inst;
+wire [31:0] jalrPc, branchPc, jalPc;    // Set by Target Gen that operates on DE_EX data
+wire [31:0] aluRes; 
+wire [31:0] pcSel;                      // Set by Branch Gen that operators on DE_EX data
+instr_t ex_mem_inst;
+logic [31:0] ex_mem_aluRes;
+
 
 always_ff @(posedge CLK) begin
-    de_ex_ir           <= if_de_ir;
-    de_ex_pc           <= if_de_pc;
-    de_ex_aluAIn       <= aluAIn;
-    de_ex_aluBIn       <= aluBIn;
-    de_ex_inst         <= if_de_inst;
-    de_ex_iTypeImmed   <= iTypeImmed;
-    de_ex_inst.invalid <= invalidate;
-   
+    ex_mem_inst     <= de_ex_inst;
+    if (!de_ex_inst.invalid) begin
+        ex_mem_aluRes   <= aluRes; // If the DE_EX stage has been invalidated, future stages must be no-ops
+    end
 end
 
 
@@ -144,28 +149,21 @@ end
 // ~~~~~~~~~~~~ //
 // MEMORY STAGE //
 // ~~~~~~~~~~~~ //
-
-wire  [31:0] jalrPc, branchPc, jalPc;      
-wire  [31:0] aluRes;
-wire   [1:0] pcSel;
-logic [31:0] ex_mem_jalrPc=0, ex_mem_branchPc=0, ex_mem_jalPc=0;
-logic [31:0] ex_mem_aluRes=0;
-logic [31:0] ex_mem_pc=0;
-logic  [1:0] ex_mem_pcSel=0;               
-instr_t ex_mem_inst;
+            
+wire [31:0] memOut2;
+instr_t mem_wb_inst;
+logic [31:0] mem_wb_aluRes;
 
 always_ff @(posedge CLK) begin
-    ex_mem_aluRes       <= aluRes;
-    ex_mem_pcSel        <= pcSel;
-    ex_mem_jalrPc       <= jalrPc;
-    ex_mem_branchPc     <= branchPc;
-    ex_mem_jalPc        <= jalPc;
-    ex_mem_inst         <= de_ex_inst;
-    ex_mem_pc           <= de_ex_pc;
-    ex_mem_inst.invalid <= invalidate; // Maybe don't need this one?
-end
+    mem_wb_inst <= ex_mem_inst;
+    if (!ex_mem_inst.invalid) begin
+        mem_wb_aluRes   <= ex_mem_aluRes; // If the EX_MEM stage has been invalidated, future stages must be no-ops
+    end
+end 
+
 always_comb begin
     IOBUS_OUT <= ex_mem_inst.rs2;
+    IOBUS_ADDR <= ex_mem_aluRes;
 end
 
 // ~~~~~~~~~~~~~~~ //
@@ -174,26 +172,6 @@ end
 
 wire  [31:0] memData;   
 wire  [31:0] dataToRegWrite;
-logic [31:0] mem_wb_memData=0;
-logic [31:0] mem_wb_pc=0;
-logic [31:0] mem_wb_aluRes=0;
-instr_t mem_wb_inst;
-
-always_ff @(posedge CLK) begin
-
-    if (!ex_mem_inst.invalid) begin
-        mem_wb_aluRes  <= ex_mem_aluRes;
-    end
-    else begin
-        mem_wb_aluRes  <= 0;
-    end
-    mem_wb_inst    <= ex_mem_inst;
-    mem_wb_pc      <= ex_mem_pc;
-    mem_wb_memData <= memData;
-end
-always_comb begin
-    IOBUS_ADDR <= mem_wb_aluRes;
-end
 
 // PIPELINE CONTROL LOGIC
 
@@ -208,35 +186,35 @@ ProgCount prog_count(
     .PC_COUNT  (pc)
 );
 Mult4to1 prog_count_next_mux(
-    .In1  (pc + 4),             // Option 1: Current PC + 4
-    .In2  (ex_mem_jalrPc),      // Option 2: JALR TARGET calculated during MEM STAGE
-    .In3  (ex_mem_branchPc),    // Option 3: BRANCH TARGET calculated during BRANCH STAGE
-    .In4  (ex_mem_jalPc),       // Option 4: JAL TARGET calculated during MEM STAGE
-    .Sel  (ex_mem_pcSel),       // Determined via PC_SEL calculated during MEM STAGE
+    .In1  (pc + 4),      // Option 1: Current PC + 4
+    .In2  (jalrPc),      // Option 2: JALR TARGET calculated during EX STAGE
+    .In3  (branchPc),    // Option 3: BRANCH TARGET calculated during EX STAGE
+    .In4  (jalPc),       // Option 4: JAL TARGET calculated during EX STAGE
+    .Sel  (pcSel),       // Determined via PC_SEL calculated during EX STAGE
     .Out  (nextPc)              
 );    
 
 OTTER_mem_byte mem(
     .MEM_CLK     (CLK),
-    .MEM_ADDR1   (pc),                        // Current PC -> IR
+    .MEM_ADDR1   (if_de_inst.pc),              
     .MEM_ADDR2   (ex_mem_aluRes),             // Access memory at the location corresponding to the ALU RESULT during the MEM STAGE 
     .MEM_DIN2    (ex_mem_inst.rs2),           // Save the value from register 2 during the MEM STAGE
     .MEM_WRITE2  ((!ex_mem_inst.invalid) & ex_mem_inst.memWrite),     
     .MEM_READ1   (~stallIf),                  // An instruction can only be fetched from MEM1 if STALL_FETCH signal is not given
     .MEM_READ2   (ex_mem_inst.memRead2),      // MEM2 can only be read from if command is LOAD
     .IO_IN       (IOBUS_IN),                 
-    .MEM_SIZE    (ex_mem_inst.memType[1:0]),
-    .MEM_SIGN    (ex_mem_inst.memType[2]),
+    .MEM_SIZE    (ex_mem_inst.func3[1:0]),
+    .MEM_SIGN    (ex_mem_inst.func3[2]),
     .ERR         (),
     .MEM_DOUT1   (ir),                        // Output of MEM1 is the current instruction
-    .MEM_DOUT2   (memData),                   // Output of MEM2 is the requested data
+    .MEM_DOUT2   (memOut2),                   // Output of MEM2 is the requested data
     .IO_WR       (IOBUS_WR)
 );
 
 OTTER_CU_Decoder decoder(
-    .CU_OPCODE     (if_de_ir[6:0]),      // Given from IR @ DECODE STAGE
-    .CU_FUNC3      (if_de_ir[14:12]),    // Given from IR @ DECODE STAGE
-    .CU_FUNC7      (if_de_ir[31:25]),    // Given from IR @ DECODE STAGE
+    .CU_OPCODE     (de_inst.ir[6:0]),      // Given from IR @ DECODE STAGE
+    .CU_FUNC3      (de_inst.ir[14:12]),    // Given from IR @ DECODE STAGE
+    .CU_FUNC7      (de_inst.ir[31:25]),    // Given from IR @ DECODE STAGE
     .CU_ALU_SRCA   (aluSrcA),            // Used for DECODE STAGE logic
     .CU_ALU_SRCB   (aluSrcB),            // Used for DECODE STAGE logic
     .CU_ALU_FUN    (aluFun),             // Used for EXECUTE STAGE logic
@@ -244,8 +222,8 @@ OTTER_CU_Decoder decoder(
 );
 
 OTTER_registerFile reg_file(
-    .READ1         (if_de_inst.rfAddr1),        
-    .READ2         (if_de_inst.rfAddr2),      
+    .READ1         (de_inst.rfAddr1),        
+    .READ2         (de_inst.rfAddr2),      
     .DEST_REG      (mem_wb_inst.rd),       
     .DIN           (dataToRegWrite),        
     .WRITE_ENABLE  ((!mem_wb_inst.invalid) && mem_wb_inst.regWrite),  
@@ -254,46 +232,46 @@ OTTER_registerFile reg_file(
     .CLK           (CLK)
 );
 Mult4to1 reg_file_data_mux(
-    .In1  (mem_wb_pc + 4),             // Option 1: PC from WRITEBACK STAGE + 4
+    .In1  (mem_wb_inst.pc + 4),             // Option 1: PC from WRITEBACK STAGE + 4
     .In2  (0),                         // Option 2: NONE
-    .In3  (mem_wb_memData),            // Option 3: Output of MEM1 from WRITEBACK STAGE
+    .In3  (memOut2),            // Option 3: Output of MEM1 from WRITEBACK STAGE
     .In4  (mem_wb_aluRes),             // Option 4: Output of ALU from WRITEBACK STAGE
-    .Sel  (ex_mem_inst.rfWrSel),     
+    .Sel  (mem_wb_inst.rfWrSel),     
     .Out  (dataToRegWrite)
 );
 
 OTTER_ALU alu(
     .ALU_FUN  (de_ex_inst.aluFun),  // Given from inst package @ EXECUTE STAGE
-    .A        (aluAIn),       
-    .B        (aluBIn),
+    .A        (de_ex_aluAIn),       
+    .B        (de_ex_aluBIn),
     .ALU_OUT  (aluRes)
 );
 Mult2to1 alu_ain_mux(
-    .In1  (rs1),
+    .In1  (de_inst.rs1),
     .In2  (uTypeImmed),
     .Sel  (aluSrcA),
     .Out  (aluAIn)
 );
 Mult4to1 alu_bin_mux(
-    .In1  (rs2),
+    .In1  (de_inst.rs2),
     .In2  (iTypeImmed),
     .In3  (sTypeImmed),
-    .In4  (if_de_pc),
+    .In4  (de_inst.pc),
     .Sel  (aluSrcB),
     .Out  (aluBIn)
 );
     
 target_gen target_gen(
-    .RS1        (rs1),            
+    .RS1        (de_ex_rs1),            
     .I_IMMED    (de_ex_iTypeImmed), 
-    .PC         (de_ex_pc),      // Is this right? (de_ex_pc)
-    .IR         (de_ex_ir),      // Is this right? (de_ex_ir)
+    .PC         (de_ex_inst.pc),   
+    .IR         (de_ex_inst.ir),     
     .JALR_PC    (jalrPc),
     .BRANCH_PC  (branchPc),
     .JUMP_PC    (jalPc)
 );
 immed_gen immed_gen(
-    .IR            (if_de_ir),
+    .IR            (de_inst.ir),
     .S_TYPE_IMMED  (sTypeImmed),
     .I_TYPE_IMMED  (iTypeImmed),
     .U_TYPE_IMMED  (uTypeImmed)
@@ -306,6 +284,7 @@ branch_cond_gen branch_cond_gen(
     .PC_SEL  (pcSel)
 );
 
+// CHECK INPUTS, MAYBE WRONG? //
 hazard_detector hazard_detector(
     .CLK            (CLK),
     .EX_MEM_RD      (ex_mem_inst.rd),
