3bb5900d76e5ac9d67b1f5c2c411fcf90965dc51
jarmeister99 <jarmeister99@gmail.com>
2020-01-27 17:12:20 -0800

    basic stalling

--- otter.srcs/sources_1/new/hazard_detector.sv
+++ otter.srcs/sources_1/new/hazard_detector.sv
@@ -24,8 +24,9 @@ module hazard_detector(
     input CLK,
     input [31:0] EX_MEM_RD,
     input [31:0] MEM_WB_RD,
-    input [31:0] ID_EX_RS1,
-    input [31:0] ID_EX_RS2,
+    input [31:0] DE_EX_RS1,
+    input [31:0] DE_EX_RS2,
+    input  [1:0] EX_MEM_PC_SEL,
     output logic STALL_IF,
     output logic STALL_DE,
     output logic STALL_EX,
@@ -38,13 +39,41 @@ module hazard_detector(
     output logic INVALID_WB
 );
 always_ff @(posedge CLK) begin
-    if (EX_MEM_RD == ID_EX_RS1) begin
+    STALL_IF  <= 0;
+    STALL_DE  <= 0;
+    STALL_EX  <= 0;
+    STALL_MEM <= 0;
+    STALL_WB  <= 0;
+    
+    // If the first output (data) of the REG_FILE at the DECODE STAGE is equal to
+    // ... the register to save data to in the EXECUTE STAGE 
+    // Then stall IF, DE, EX
+    if (EX_MEM_RD == DE_EX_RS1) begin
+        STALL_IF <= 1;
+        STALL_DE <= 1;
+        STALL_EX <= 1;
     end
-    if (EX_MEM_RD == ID_EX_RS2) begin
+    // ...
+    /// Stall IF, DE, EX
+    if (EX_MEM_RD == DE_EX_RS2) begin
+        STALL_IF <= 1;
+        STALL_DE <= 1;
+        STALL_EX <= 1;
     end
-    if (MEM_WB_RD == ID_EX_RS1) begin
+    // If the first output (data) of the REG_FILE at the DECODE STAGE is equal to
+    // ... the register to save data to in the MEMORY STAGE
+    // Then stall IF, DE, EX, MEM
+    if (MEM_WB_RD == DE_EX_RS1) begin
+        STALL_IF  <= 1;
+        STALL_DE  <= 1;
+        STALL_EX  <= 1;
+        STALL_MEM <= 1;
     end
-    if (MEM_WB_RD == ID_EX_RS2) begin
+    if (MEM_WB_RD == DE_EX_RS2) begin
+        STALL_IF  <= 1;
+        STALL_DE  <= 1;
+        STALL_EX  <= 1;
+        STALL_MEM <= 1;
     end
 end
 endmodule
