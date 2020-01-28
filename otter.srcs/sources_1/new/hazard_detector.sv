c0fd33cf118a24c36bccf2bed25a854c4d64a30a
jarmeister99 <jarmeister99@gmail.com>
2020-01-27 17:52:12 -0800

    hazard detection bug fixes

--- otter.srcs/sources_1/new/hazard_detector.sv
+++ otter.srcs/sources_1/new/hazard_detector.sv
@@ -22,11 +22,11 @@
 
 module hazard_detector(
     input CLK,
-    input [31:0] EX_MEM_RD,
-    input [31:0] MEM_WB_RD,
-    input [31:0] DE_EX_RS1,
-    input [31:0] DE_EX_RS2,
-    input  [1:0] EX_MEM_PC_SEL,
+    input logic [4:0] EX_MEM_RD,
+    input logic [4:0] MEM_WB_RD,
+    input logic [4:0] DE_EX_RF_ADDR1,
+    input logic [4:0] DE_EX_RF_ADDR2,
+    input logic [1:0] EX_MEM_PC_SEL,
     output logic STALL_IF,
     output logic STALL_DE,
     output logic STALL_EX,
@@ -39,41 +39,58 @@ module hazard_detector(
     output logic INVALID_WB
 );
 always_ff @(posedge CLK) begin
-    STALL_IF  <= 0;
-    STALL_DE  <= 0;
-    STALL_EX  <= 0;
-    STALL_MEM <= 0;
-    STALL_WB  <= 0;
     
     // If the first output (data) of the REG_FILE at the DECODE STAGE is equal to
     // ... the register to save data to in the EXECUTE STAGE 
     // Then stall IF, DE, EX
-    if (EX_MEM_RD == DE_EX_RS1) begin
+    if (EX_MEM_RD == DE_EX_RF_ADDR1) begin
         STALL_IF <= 1;
         STALL_DE <= 1;
         STALL_EX <= 1;
     end
+    else begin
+        STALL_IF <= 0;
+        STALL_DE <= 0;
+        STALL_EX <= 0;
+    end
     // ...
     /// Stall IF, DE, EX
-    if (EX_MEM_RD == DE_EX_RS2) begin
+    if (EX_MEM_RD == DE_EX_RF_ADDR2) begin
         STALL_IF <= 1;
         STALL_DE <= 1;
         STALL_EX <= 1;
+    end
+    else begin
+        STALL_IF <= 0;
+        STALL_DE <= 0;
+        STALL_EX <= 0;
     end
     // If the first output (data) of the REG_FILE at the DECODE STAGE is equal to
     // ... the register to save data to in the MEMORY STAGE
     // Then stall IF, DE, EX, MEM
-    if (MEM_WB_RD == DE_EX_RS1) begin
+    if (MEM_WB_RD == DE_EX_RF_ADDR1) begin
         STALL_IF  <= 1;
         STALL_DE  <= 1;
         STALL_EX  <= 1;
         STALL_MEM <= 1;
     end
-    if (MEM_WB_RD == DE_EX_RS2) begin
+    else begin
+        STALL_IF  <= 0;
+        STALL_DE  <= 0;
+        STALL_EX  <= 0;
+        STALL_MEM <= 0;
+    end
+    if (MEM_WB_RD == DE_EX_RF_ADDR2) begin
         STALL_IF  <= 1;
         STALL_DE  <= 1;
         STALL_EX  <= 1;
         STALL_MEM <= 1;
+    end
+    else begin
+        STALL_IF  <= 0;
+        STALL_DE  <= 0;
+        STALL_EX  <= 0;
+        STALL_MEM <= 0;
     end
 end
 endmodule
